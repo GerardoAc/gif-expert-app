46920949c02a3acb08d4d4f0e2f53c6bbb08e7da
Gerardo Araya <gerardo.araya@pandatechla.com>
2024-03-27 15:29:20 -0600

    Se agrega lista

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,22 @@
-
+import { useState } from 'react';
 
 export const GifExpertApp = () => {
-  return (
-    <>
-        <h1>Gift APP</h1>
-    </>
-  )
+    const [categories, setCategories] = useState(['Anime', 'Carros']);
+    const onAddCategories = () => {
+        setCategories([...categories, 'categ']);
+    }
+    return (
+        <>
+            <h1>Gift APP</h1>
+            <ol>
+                <input placeholder='categorias'
+                    name='cat'/>
+                {categories.map(category => {
+                    return <li key={category}>{category}</li>
+                })}
+                <button onClick={onAddCategories}>Aggregar</button>
+            </ol>
+        </>
+    )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
-import  ReactDOM  from 'react-dom/client'
-import {GifExpertApp} from './GifExpertApp'
+import React from 'react';
+import  ReactDOM  from 'react-dom/client';
+import {GifExpertApp} from './GifExpertApp';
+import './styless.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
+  //<!--1x4v3YgePN91WY65JynVmnWxZ1tVkbGv>
   <React.StrictMode>
     <GifExpertApp />
   </React.StrictMode>,
